cb558b014fca70b22c432623b158186d5c5eb268
FunnyKing1228 <101647230+FunnyKing1228@users.noreply.github.com>
2025-06-09 15:21:40 +0800

    Fix package directory in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='microgrid-fedrl',
     version='0.1.0',
-    package_dir={'': 'microgrid-fedrl'},
-    packages=find_packages('microgrid-fedrl'),
+    package_dir={'': 'microgrid-fedrl/src'},
+    packages=find_packages('microgrid-fedrl/src'),
     install_requires=['pyyaml'],
 )
